bf6ca57cea0eda7ed4b3a219efc7126e4aa6d23a
jarwo9 <hasansarjowo@gmail.com>
2024-11-16 02:20:05 +0700

    Update x.sh

--- x.sh
+++ x.sh
@@ -1,1 +1,8 @@
-k
+python -m pip install pip --upgrade
+pip install requests
+pip install asyncio
+pip install loguru
+pip install websockets_proxy
+pip install fake_useragent
+pip uninstall websockets
+pip install websockets==12.0
